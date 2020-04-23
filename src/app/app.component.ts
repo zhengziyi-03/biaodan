7472f94f2b312e8caf14920a0dc0e9684de38784
zhengziyi <1473498140@qq.com>
2020-04-23 17:03:21 +0800

    biaodan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, AbstractControl, Validators, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+function userNameValidator(control: FormControl):{ [s: string]: boolean} {
+  if (!control.value.match(/^a/)) {
+    return {invalidUser: true };
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'login-app';
+  myForm: FormGroup;
+
+  userName: AbstractControl;
+
+  password: AbstractControl;
+
+  name$: Observable<string>;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group(
+      {
+        'userName': ['aaa',Validators.compose([Validators.required, userNameValidator])],
+        'password': ['',Validators.compose([Validators.required, Validators.minLength(5)])]
+      }
+    );
+
+    this.userName = this.myForm.controls['userName'];
+    this.password = this.myForm.controls['password'];
+    this.name$ = this.userName.valueChanges;
+  }
+  onSubmit(value: any) {
+    console.log(value);
+  }
 }
